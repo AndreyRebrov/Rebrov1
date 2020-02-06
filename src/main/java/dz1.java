77806bd3ae98f94b346c27e0b4ac3350824f20e7
Andrey Rebrov <atalay08@mail.ru>
2020-02-06 05:04:19 +0300

    v011

--- src/main/java/dz1.java
+++ src/main/java/dz1.java
@@ -1,4 +1,7 @@
-public class dz1 {
+package lesson1.homeans;
+
+public class dz01_1 {
+
     //Создаем пустой проект в IntelliJ IDEA и прописываем метод main
     public static void main(String[] args) {
 
@@ -21,5 +24,95 @@ public class dz1 {
         System.out.println("Значение тиипа char = " + c);
         System.out.println("Значение тиипа bookean = " + bool);
     }
+}
 
+
+public class dz1_1 {
+    public static void main(String[] args) {
+        //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        //где a, b, c, d – аргументы этого метода, имеющие тип float.
+        int a = 4, b = 12, c = 76, d = 7;
+        int e = a * (b + (c / d));
+        System.out.println((float) e);
+
+
+    }
+}
+
+
+import java.util.Scanner;
+
+public class dz1_2 {
+    public static void main(String[] args) {
+        //Написать метод, принимающий на вход два целых числа и проверяющий,
+        // что их сумма лежит в пределах от 10 до 20 (включительно),
+        // если да – вернуть true, в противном случае – false.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = in.nextInt();
+        System.out.println("Введите второе число");
+        int b = in.nextInt();
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+            System.out.println("true");
+        } else
+            System.out.println("false");
+    }
+}
+
+
+
+import java.util.Scanner;
+
+public class dz1_3 {
+    public static void main(String[] args) {
+        //Написать метод, которому в качестве параметра передается целое число,
+        // метод должен напечатать в консоль, положительное ли число передали
+        // или отрицательное. Замечание: ноль считаем положительным числом.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = in.nextInt();
+        if (a <= -1) {
+            System.out.println("Число отрицательное");
+        }
+        else {
+            System.out.println("Число Положительное");
+        }
+    }
+}
+
+
+
+
+import java.util.Scanner;
+
+public class dz1_4 {
+    public static void main(String[] args) {
+        //  Написать метод, которому в качестве параметра передается целое число.
+        //  Метод должен вернуть true, если число отрицательное.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = in.nextInt();
+        if (a < 0)
+            System.out.println(true);
+        else;
+        System.out.println(false);
+
+
+    }
+}
+
+
+import java.util.Scanner;
+
+public class dz_5 {
+
+
+    //Написать метод, которому в качестве параметра передается строка, обозначающая имя,
+    // метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("Привет," + a);
+    }
 }
