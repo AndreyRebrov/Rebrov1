283bfa8b33bcd0af67a56adb9317bc8eaa126720
Andrey Rebrov <atalay08@mail.ru>
2020-02-06 04:39:57 +0300

    Initial commit

---
+++ src/main/java/dz1_1.java
@@ -0,0 +1,17 @@
+public class dz1_1 {
+    public static void main(String[] args) {
+        //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        //где a, b, c, d – аргументы этого метода, имеющие тип float.
+        int a = 4,  b = 12,  c =76,  d = 7;
+        int e = a * (b + (c / d));
+        System.out.println((float)e);
+
+
+    }
+
+
+
+
+
+
+}
